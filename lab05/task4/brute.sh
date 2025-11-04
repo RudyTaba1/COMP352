fcb05ea8a4bc6121c8bec281c0fd37253c3f8730
RudyTaba1 <rudytaba04@gmail.com>
2025-11-04 01:46:41 -0500

    adding rest of lab

---
+++ lab05/task4/brute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SECONDS=0
+
+value=0
+
+
+while [ 1 ]
+	do
+		value=$(( $value + 1 ))
+		duration=$SECONDS
+		min=$(($duration/60))
+		sec=$(($duration%60))
+		echo"$min minutes and $sec seconds elapsed."
+		echo"THe Program has been running $value times so far."
+		./stack1 && break
+	done
