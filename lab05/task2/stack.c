965e9947153ac905e593d2c686817915bc15fdbd
RudyTaba1 <rudytaba04@gmail.com>
2025-11-03 23:52:01 -0500

    Finally figured out lab05, task2. executing into shell.

---
+++ lab05/task2/stack.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef BUF_SIZE
+#define BUF_SIZE 24
+#endif
+
+int bof(char *str){
+	
+     char buffer[BUF_SIZE];
+
+     /*The Following statement has a buffer overflow*/
+     strcpy(buffer, str);
+
+     return 1;
+
+}
+
+int main(int argc, char **argv){
+     
+     char str[517];
+     FILE *badfile;
+
+     /*Change the size of this dummy array to the randomize parameters for this       lab. Need to use the array at least once.*/
+     char dummy[BUF_SIZE]; memset(dummy, 0, BUF_SIZE);
+
+     badfile = fopen("badfile", "r");
+     fread(str, sizeof(char), 517, badfile);
+     bof(str);
+     printf("Returned Properly \n");
+     return 1;
+}
